ecffe1326e6c212e3ec09ce5a0a24745a693ea45
Logan Woolf <loganwoolf@gmail.com>
2021-11-18 07:05:54 +0000

    create packing list

---
+++ packing-list.js
@@ -0,0 +1,7 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+for (let i of packingList) {
+  console.log(i);
+}
+
